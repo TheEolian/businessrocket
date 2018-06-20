ea2b773792ec55ac662a32ae22d42d4c6c88242c
Michael Soldmann <msoldmann@gmail.com>
2018-06-20 17:09:30 -0500

    added google maps API to contact section

--- js/init.js
+++ js/init.js
@@ -24,5 +24,37 @@ $(document).ready(function () {
 
   Materialize.scrollFire(options);
 
+  const position = [42.098952, -88.278527];
+      const centerPosition = [42.103458, -88.189805];
+
+      function showGoogleMaps() {
+        // Map positions
+        const latLng = new google.maps.LatLng(position[0], position[1]);
+        const centerLatLng = new google.maps.LatLng(centerPosition[0], centerPosition[1]);
+
+        // Map options
+        const mapOptions = {
+          zoom: 12,
+          streetViewControl: false,
+          scaleControl: true,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: centerLatLng
+        }
+
+        // Create map
+        map = new google.maps.Map(document.getElementById('googlemap'), mapOptions);
+
+        // Show Marker
+        marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          draggable: false,
+          animation: google.maps.Animation.DROP
+        });
+      }
+
+      // Maps event
+      google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
 
 });
